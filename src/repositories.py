8d5937a6b3def5e492f4145e14769040f4fd97da
Bart3kD <u59_bardeu_waw@technischools.com>
2024-02-18 19:42:14 +0100

    Some thing

---
+++ src/repositories.py
@@ -0,0 +1,50 @@
+from src.users import User
+
+users = []
+
+
+class UserRepository:
+    def create(self, user_data):
+        user_id = len(users) + 1
+        user = User(
+            user_id,
+            user_data["firstName"],
+            user_data["lastName"],
+            user_data["birthYear"],
+            user_data["group"]
+        )
+        users.append(user)
+        return user
+
+
+    def get_all(self):
+        return users
+
+
+    def get_by_id(self, user_id):
+        for user in users:
+            if user.id == user_id:
+                return user
+        return None
+
+
+    def update(self, user_id, user_data):
+        user = self.get_by_id(user_id)
+        if user:
+            user.first_name = user_data.get("firstName", user.first_name)
+            user.last_name = user_data.get("lastName", user.last_name)
+            user.birth_year = user_data.get("birthYear", user.birth_year)
+
+            if "group" in user_data:
+                user.group = user_data["group"]
+
+            return user
+        return None
+
+
+    def delete(self, user_id):
+        user = self.get_by_id(user_id)
+        if user:
+            users.remove(user)
+            return True
+        return False
---
+++ src/users.py
@@ -0,0 +1,7 @@
+class User:
+    def __init__(self, user_id, first_name, last_name, birth_year, group):
+        self.id = user_id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.birth_year = birth_year
+        self.group = group
