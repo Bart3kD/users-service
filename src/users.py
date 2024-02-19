6317e43ec9b2269b0d243b9e333aef1d88ba7322
Bart3kD <u59_bardeu_waw@technischools.com>
2024-02-19 15:56:10 +0100

    users change

--- src/users.py
+++ src/users.py
@@ -1,7 +1,21 @@
 class User:
+    ALLOWED_GROUPS = {"user", "premium", "admin"}
+
     def __init__(self, user_id, first_name, last_name, birth_year, group):
         self.id = user_id
         self.first_name = first_name
         self.last_name = last_name
         self.birth_year = birth_year
+        self._group = None 
+
         self.group = group
+
+    @property
+    def group(self):
+        return self._group
+
+    @group.setter
+    def group(self, value):
+        if value not in self.ALLOWED_GROUPS:
+            raise ValueError(f"Invalid group: {value}. Allowed values: {self.ALLOWED_GROUPS}")
+        self._group = value
